553dd0481c2528ece11e854ee48e8ffe5259d438
varunsh17 <varunsh170502@gmail.com>
2021-06-19 19:48:22 +0530

    rain drops

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
     <Navbar/>
+
     <Home/>
     <Events/>
     <News/>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import {Container,Typography,Grid ,makeStyles, Paper} from "@material-ui/core"
-import rain from "./img/rain.jpg"
+import "./style.css"
 
-const useStyles=makeStyles((theme)=>({
-    Container:{
-        backgroundImage: `url(${rain})`,
-        backgroundSize:"cover",   
-        maxWidth:"100vw",
-        height:"100vh",
-       
-    }
 
-}));
 function Home() {
-    const classes=useStyles()
 
     return (
-        <Paper className={classes.Container}>
-            Home
-        </Paper>
+        <div className="Container">
+        <div id="raincontainer" className="rain-container" >
+        <div className="drop">
+            <div className="stem">
+                <div className="splash">
+                
+                </div>
+            </div>
+        </div>
+            
+            </div>
+        </div>
     )
 }
 
