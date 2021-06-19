73209ab2be6d44d810a7bc1af409f66b8fef2bf1
rakeshhotker <rakeshhotker@gmail.com>
2021-06-19 20:11:11 +0530

    few changes to test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import {Container,Typography,Grid ,makeStyles, Paper} from "@material-ui/core"
-import rain from "./img/rain.jpg"
+import React from 'react';
+import {
+	Container,
+	Typography,
+	Grid,
+	makeStyles,
+	Paper,
+} from '@material-ui/core';
+import rain from './img/rain.jpg';
 
-const useStyles=makeStyles((theme)=>({
-    Container:{
-        backgroundImage: `url(${rain})`,
-        backgroundSize:"cover",   
-        maxWidth:"100vw",
-        height:"100vh",
-       
-    }
-
+const useStyles = makeStyles((theme) => ({
+	Container: {
+		backgroundImage: `url(${rain})`,
+		backgroundSize: 'cover',
+		maxWidth: '100vw',
+		height: '100vh',
+	},
 }));
 function Home() {
-    const classes=useStyles()
+	const classes = useStyles();
 
-    return (
-        <Paper className={classes.Container}>
-            Home
-        </Paper>
-    )
+	return <Paper className={classes.Container}>{/* Home */}</Paper>;
 }
 
-export default Home
+export default Home;
