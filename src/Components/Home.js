ac7f7b05cb03ec733e46c1a96adc18b5cf9b42ae
rakeshhotker <rakeshhotker@gmail.com>
2021-06-23 01:12:17 +0530

    footer in home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,31 +8,31 @@ import Login from './Login';
 import News from './News';
 import Events from './Events';
 import Fotter from './Fotter';
-function Home(){
-	const [isOpen,setIsOpen]=useState(false);
-	
-	const toggle=()=>{
+function Home() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => {
 		setIsOpen(!isOpen);
-	}
-		return (
-			<>
-					<Sidebar isOpen={isOpen} toggle={toggle}/>
-					<Navbar toggle={toggle}/>
-					<div className='body'>
-						<div className='main'></div>
-					</div>
-					{/* ADDED MOTION ON HOME PAGE - VARUN */}
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 0.6 }}
-						transition={{ duration: 10, yoyo: Infinity }}
-						className='another1'
-					></motion.div>
-					<Events />
-					<News />
-					<Login />
-							<Fotter/>
-			</>
-		);
-	}
+	};
+	return (
+		<>
+			<Sidebar isOpen={isOpen} toggle={toggle} />
+			<Navbar toggle={toggle} />
+			<div className='body'>
+				<div className='main'></div>
+			</div>
+			{/* ADDED MOTION ON HOME PAGE - VARUN */}
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 0.6 }}
+				transition={{ duration: 10, yoyo: Infinity }}
+				className='another1'
+			></motion.div>
+			<Events />
+			<News />
+			<Login />
+			<Fotter /> {/*footer is in home page*/}
+		</>
+	);
+}
 export default Home;
