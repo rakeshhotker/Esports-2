33509b91c78ab0ad86467b72aec21af76daeb8e3
rakeshhotker <rakeshhotker@gmail.com>
2021-06-26 17:41:48 +0530

    made tweaks but no change

--- src/Components/Navbar/NavElements.js
+++ src/Components/Navbar/NavElements.js
@@ -4,7 +4,7 @@ import { Link as LinkS } from 'react-scroll';
 export const Nav = styled.nav`
 	background: rgba(0, 0, 0, 0.4);
 	height: 50px;
-	margin-top: -80px;
+	margin-top: -60px;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -62,14 +62,14 @@ export const NavMenu = styled.ul`
 	}
 `;
 export const NavItem = styled.li`
-	height: 60px;
+	height: 50px;
 `;
 export const NavLinks = styled(LinkS)`
 	color: #fff;
 	display: flex;
 	align-items: center;
 	text-decoration: none;
-	padding: 0 1rem;
+	padding: 0 2rem;
 	height: 100%;
 	cursor: pointer;
 	&.active {
@@ -89,7 +89,7 @@ export const NavBtnLink = styled(LinkR)`
 	white-space: nowrap;
 	padding: 10px 22px;
 	color: #010606;
-	font-size: 18px;
+	font-size: 14px;
 	outline: none;
 	border: none;
 	cursor: pointer;
